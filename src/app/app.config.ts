2d17e87e1037fb6ba8316b117f2cf0afde6cb665
Araygoza099 <araygoza099@gmail.com>
2024-06-15 10:08:25 -0600

    Firebase
    Hagan npm install
    npm install firebase
    npm install -g firebase-tools
    firebase login
    firebase init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,33 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, provideAuth } from "@angular/fire/auth";
+import { provideFirebaseApp } from "@angular/fire/app";
+
+
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAQy_3GIgHNxUF3HteNubgasuz6wJxrHNE",
+  authDomain: "pf-rap.firebaseapp.com",
+  projectId: "pf-rap",
+  storageBucket: "pf-rap.appspot.com",
+  messagingSenderId: "296975379675",
+  appId: "1:296975379675:web:dd6f0f933e85d3c22070fe",
+  measurementId: "G-L1LGCRHZDN"
+}; 
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
-};
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+  provideHttpClient(),provideRouter(routes), provideAnimationsAsync(), (
+  [
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+  ]
+  )]
+  };
+
